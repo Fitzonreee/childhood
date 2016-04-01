e9127138fdf922dc0c7b4f66812aaa7c5e2af72c
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-04-01 07:38:05 -0700

    Added Google Font and added dates to pictures, minor styling

--- index.php
+++ index.php
@@ -3,36 +3,46 @@
   <head>
     <meta charset="utf-8">
     <title>Childhood</title>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="dist/css/c.min.css" media="screen">
   </head>
   <body>
     <div class="grid">
       <div class="grid-sizer"></div>
       <div class="grid-item">
+        <h1>1989</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" />
       </div>
       <div class="grid-item">
+        <h1>1992</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />
       </div>
       <div class="grid-item">
+        <h1>1995</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
       </div>
       <div class="grid-item">
+        <h1>1997</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
       </div>
       <div class="grid-item">
+        <h1>2003</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
       </div>
       <div class="grid-item">
+        <h1>2005</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
       </div>
       <div class="grid-item">
+        <h1>2007</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
       </div>
       <div class="grid-item">
+        <h1>2011</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
       </div>
       <div class="grid-item">
+        <h1>2016</h1>
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
       </div>
     </div>

99673a09eee6bf3a910df894a193a9456fe53b28
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-04-02 12:18:52 -0700

    Added bg to each img div to resemble polaroid

--- index.php
+++ index.php
@@ -9,46 +9,84 @@
   <body>
     <div class="grid">
       <div class="grid-sizer"></div>
+
       <div class="grid-item">
-        <h1>1989</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" />
+        <div class="item-content">
+          <h1>1989</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>1992</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />
+        <div class="item-content">
+          <h1>1989</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>1995</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
+        <div class="item-content">
+          <h1>1992</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>1997</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
+        <div class="item-content">
+          <h1>1995</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>2003</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
+        <div class="item-content">
+          <h1>1997</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>2005</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+        <div class="item-content">
+          <h1>2003</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>2007</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
+        <div class="item-content">
+          <h1>2005</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>2011</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
+        <div class="item-content">
+          <h1>2007</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>2016</h1>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+        <div class="item-content">
+          <h1>2011</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
+        </div>
       </div>
+
       <div class="grid-item">
-        <h1>2013</h1>
-        <img src="dist/img/goofball_DMA.jpg" class="ig-earlybird"/>
+        <div class="item-content">
+          <h1>2016</h1>
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+        </div>
       </div>
+
+      <div class="grid-item">
+        <div class="item-content">
+          <h1>2013</h1>
+          <img src="dist/img/goofball_DMA.jpg" class="ig-earlybird"/>
+        </div>
+      </div>
+
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script type="text/javascript" src="dist/js/j.js"></script>
